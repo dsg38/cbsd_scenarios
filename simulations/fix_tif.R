f5cea510ddc42c49560e647b012a97ca2e7d9c74
David Godding <dgodding7@gmail.com>
2021-03-19 11:02:25 -0500

    Improved tif script

--- simulations/fix_tif.R
+++ simulations/fix_tif.R
@@ -1,45 +1,43 @@
-topDir = "./sim_output/2021_03_17_cross_continental/2021_03_18_batch_0/"
-endYear = 2050
+topDir = "./sim_output/2021_03_17_cross_continental/"
 
 # Get only last ones to avoid sims in prog
-lastRasterPaths = list.files(topDir, pattern=paste0("O_0_L_0_INFECTIOUS_", endYear, ".000000.txt"), full.names = T, recursive = T)
-
-# HACK TO MINIMISE CHANCE OF CONFLICT
-lastRasterPaths = sample(lastRasterPaths)
+txtRasterPaths = list.files(topDir, pattern="O_0_L_0_INFECTIOUS_.*.000000.txt", full.names = T, recursive = T)
 
 fixCount = 0
-for(thisLastRasterPath in lastRasterPaths){
+while(length(txtRasterPaths) > 0){
+    
+    # Pick one
+    thisRasterPath = sample(txtRasterPaths, 1)
+    
+    # Check file old enough
+    rasterCreated = file.info(thisRasterPath)$ctime
+    timeDiff = Sys.time() - rasterCreated
+    numSecs = as.numeric(timeDiff, units = "secs")
+    
+    if(numSecs > 1000){
         
-    thisResultsDir = dirname(thisLastRasterPath)
-    
-    print(thisResultsDir)
-    
-    allRasterPaths = list.files(thisResultsDir, pattern="O_0_L_0_INFECTIOUS_.*.000000.txt", full.names = T)
-    
-    # HACK SO I CAN SPAWN MULTIPLE
-    if(file.path(thisResultsDir, "O_0_L_0_INFECTIOUS_2004.000000.txt") %in% allRasterPaths){
+        print(thisRasterPath)
         
-        for(thisRasterPath in allRasterPaths){
-            
-            print(thisRasterPath)
-            
-            # Read raster
-            thisRaster = raster::raster(thisRasterPath)
-            
-            outRasterPath = gsub(".txt", ".tif", thisRasterPath)
-            
-            # Write out as tif
-            raster::writeRaster(thisRaster, outRasterPath, overwrite=TRUE)
-            
-            # Delete old raster
-            file.remove(thisRasterPath)
-            
-            fixCount = fixCount + 1
-            
-            print(fixCount)
-            
-        }
+        # Read raster
+        thisRaster = raster::raster(thisRasterPath)
+        
+        outRasterPath = gsub(".txt", ".tif", thisRasterPath)
+        
+        # Write out as tif
+        raster::writeRaster(thisRaster, outRasterPath, overwrite=TRUE)
+        
+        # Delete old raster
+        file.remove(thisRasterPath)
+        
+        fixCount = fixCount + 1
+        print(fixCount)
         
     }
+    
+    # Regen list of rasters
+    txtRasterPaths = list.files(topDir, pattern="O_0_L_0_INFECTIOUS_.*.000000.txt", full.names = T, recursive = T)
+    
+    print("NUM LEFT TO FIX:")
+    print(length(txtRasterPaths))
     
 }
