3ffef4446e85af617c00a55c673cc4e9eb149de2
David Godding <dgodding7@gmail.com>
2021-03-23 15:21:59 -0500

    Converting inf to num fields raster

--- simulations/build_num_inf_fields_raster.R
+++ simulations/build_num_inf_fields_raster.R
@@ -1,14 +1,13 @@
 options(stringsAsFactors = FALSE)
-# infRasterDir = "./sim_output/2021_03_17_cross_continental/2021_03_18_batch_0/job0/"
-# hostRasterPath = "../inputs/inputs_scenarios/2021_03_17_cross_continental/inputs/L_0_HOSTDENSITY.txt"
+infRasterDir = "./sim_output/2021_03_17_cross_continental/2021_03_18_batch_0/job330/"
+hostRasterPath = "../inputs/inputs_scenarios/2021_03_17_cross_continental/inputs/L_0_HOSTDENSITY.txt"
 
-infRasterDir = "/rds/project/cag1/rds-cag1-general/epidem-userspaces/dsg38/cbsd_scenarios/simulations/sim_output/2021_03_21_nigeria_resistant/2021_03_21_batch_1/job11/"
-hostRasterPath = "../inputs/inputs_scenarios/2021_03_18_nigeria_region/inputs/L_0_HOSTDENSITY.txt"
-
+# infRasterDir = "/rds/project/cag1/rds-cag1-general/epidem-userspaces/dsg38/cbsd_scenarios/simulations/sim_output/2021_03_21_nigeria_resistant/2021_03_21_batch_1/job0/"
+# hostRasterPath = "../inputs/inputs_scenarios/2021_03_18_nigeria_region/inputs/L_0_HOSTDENSITY.txt"
 
 # -------------------------------------------------------------------
 
-infRasterPaths = list.files(infRasterDir, pattern="O_0_L_0_INFECTIOUS_.*.000000.txt", full.names = T, recursive = T)
+infRasterPaths = list.files(infRasterDir, pattern="O_0_L_0_INFECTIOUS_.*.000000.tif", full.names = T, recursive = T)
 
 hostRaster = raster::raster(hostRasterPath)
 
@@ -17,7 +16,7 @@ for(infRasterPath in infRasterPaths){
     print(infRasterPath)
     
     # Out path
-    outPathRaster = gsub('.txt', '_NUMFIELDS.tif', infRasterPath)
+    outPathRaster = gsub('.tif', '_NUMFIELDS.tif', infRasterPath)
 
     infRaster = raster::raster(infRasterPath)
 
