29956658a4607c66c30bd8209bfcf02be1bdff1b
David Godding <dgodding7@gmail.com>
2021-03-25 17:27:47 -0500

    Trying to automate chksecs

--- simulations/launch/2021_03_25_test_dptcp_launch.sh
+++ simulations/launch/2021_03_25_test_dptcp_launch.sh
@@ -4,7 +4,7 @@ module load python3
 module load pmpem
 
 python3.sh my_pmpem.py \
--o "../sim_output/2021_03_25_test_dptcp/2021_03_25_batch_0/" \
+-o "../sim_output/2021_03_25_test_dptcp/2021_03_25_batch_1/" \
 --nsamplesperparamset 10 \
 --densityfile "../../inputs/inputs_raw/params/2009_1_tol_0_65_mask_10_posterior.txt" \
 --opertype pd \
