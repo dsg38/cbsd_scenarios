f8cf29634b5ec8149c4ccf27aa3f8086963b9120
David Godding <dgodding7@gmail.com>
2021-03-25 13:37:37 -0500

    Setting up auto tif conversion in pmpem

--- simulations/launch/my_pmpem_fix_tif.R
+++ simulations/launch/my_pmpem_fix_tif.R
@@ -7,7 +7,7 @@ jobDir = args[[1]]
 
 # ----------------------------------------------------------
 
-txtRasterPaths = normalizePath(list.files(jobDir, pattern="O_*_L_0_*_.*.txt", full.names = T, recursive = T))
+txtRasterPaths = normalizePath(list.files(jobDir, pattern="O_.*_L_0_.*_.*.txt", full.names = T, recursive = T))
 
 print("R: LIST OF FILES TO FIX:")
 print(txtRasterPaths)
