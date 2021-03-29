a51bd6b1f648a2144bed6a8e7703fd1db4caead6
David Godding <dgodding7@gmail.com>
2021-03-29 12:07:11 -0500

    Setting up fixed cross continental round 2

---
+++ simulations/launch/2021_03_29_cross_continental_launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+module load python3
+module load pmpem
+
+python3.sh my_pmpem.py \
+-o "../sim_output/2021_03_26_cross_continental/2021_03_29_batch_0/" \
+--nsamplesperparamset 900 \
+--densityfile "../../inputs/inputs_raw/params/2009_1_tol_0_65_mask_10_posterior.txt" \
+--opertype pd \
+--scheduler s \
+--slurmqueue "SL2" \
+--memoryrequest 17940 \
+--runtimerequest "36:00:00" \
+--parametersfile "2021_03_17_cross_continental_params.txt" \
+--landscapefolder "/rds/project/cag1/rds-cag1-general/epidem-userspaces/dsg38/cbsd_scenarios/inputs/inputs_scenarios/2021_03_17_cross_continental/inputs" \
+--launchscript `readlink -f $0` \
+--dmtcp_restart_limit 10
