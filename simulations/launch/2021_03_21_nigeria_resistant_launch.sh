5aebd5c3afcb8e36c5db975b178a1634ff3b4970
dsg38 <dgodding7@gmail.com>
2021-04-20 17:21:34 +0100

    Minor launch script edits

--- simulations/launch/2021_03_21_nigeria_resistant_launch.sh
+++ simulations/launch/2021_03_21_nigeria_resistant_launch.sh
@@ -4,14 +4,14 @@ module load python3
 module load pmpem
 
 python3.sh my_pmpem.py \
--o "../sim_output/2021_03_21_nigeria_resistant/2021_03_21_batch_1/" \
+-o "../sim_output/2021_03_21_nigeria_resistant/2021_03_21_batch_0/" \
 --nsamplesperparamset 10 \
 --densityfile "../../inputs/inputs_raw/params/2009_1_tol_0_65_mask_10_posterior.txt" \
 --opertype pd \
 --scheduler s \
 --slurmqueue "SL2" \
 --memoryrequest 5980 \
---runtimerequest "16:00:00" \
+--runtimerequest "24:00:00" \
 --parametersfile "2021_03_21_nigeria_resistant_params.txt" \
 --landscapefolder "/rds/project/cag1/rds-cag1-general/epidem-userspaces/dsg38/cbsd_scenarios/inputs/inputs_scenarios/2021_03_18_nigeria_region/inputs" \
 --launchscript "/rds/project/cag1/rds-cag1-general/epidem-userspaces/dsg38/cbsd_scenarios/simulations/launch/2021_03_21_nigeria_resistant_launch.sh" \
