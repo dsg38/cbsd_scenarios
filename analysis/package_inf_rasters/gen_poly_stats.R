977cace036798018c686c1beb594c50e54c26af7
David Godding <dgodding7@gmail.com>
2021-05-07 12:18:44 -0500

    Profiling

--- analysis/package_inf_rasters/gen_poly_stats.R
+++ analysis/package_inf_rasters/gen_poly_stats.R
@@ -1,65 +1,52 @@
 x = peakRAM::peakRAM({
 
     box::use(tictoc[...])
-
+    
     tic(msg="total")
-
+    
     args = commandArgs(trailingOnly=TRUE)
 
     configPath = args[[1]]
     jobIndex = args[[2]]
-
-    # configPath = "./2021_03_26_cross_continental/config_poly.json"
-    # jobIndex = 0
-
+    
+    configPath = "./2021_03_26_cross_continental/config_poly.json"
+    jobIndex = 0
+    
     # ---------------------------------
     config = rjson::fromJSON(file=configPath)
-
+    
     # Job folder
     batchPath = here::here(config[["batchPath"]])
     polyDfPath = here::here(config[["polyDfPath"]])
     hostRasterPath = here::here(config[["hostRasterPath"]])
-
+    
     # ---------------------------------
-
+    
     jobDir = here::here(batchPath, paste0("job", jobIndex), "output/runfolder0")
-
+    
     # Load polys
     polyDf = sf::read_sf(polyDfPath)
-
+    
     # Read in host raster
-    hostRaster = terra::rast(hostRasterPath)
-
+    hostRaster = raster::raster(hostRasterPath)
+    
     # List all rasters
     rasterPaths = list.files(jobDir, pattern="O_0_L_0_*_.*.000000.tif", full.names = T, recursive = T)
-    rasterPaths = rasterPaths[1:2]
-
-    # Build raster stack
-    rasterStack = terra::rast(rasterPaths)
-
-    # Calculate num inf fields stack NB: for some reason, this loses the names in the stack
-    tic(msg="multiply")
-    rasterStackNumFieldsTerra = rasterStack * hostRaster * 1000
-    toc()
-
-    # Convert to raster stack for exactextractr
-    rasterStackNumFields = raster::stack(rasterStackNumFieldsTerra)
-
+    # rasterPaths = rasterPaths[1:2]
+    
     # Define func to work out num populated cells
     numCellsPopulated = function(values, coverage_fractions){
         return(sum(values>0, na.rm = TRUE))
     }
-
+    
     # Calc poly stats for each raster
     tic(msg="extract")
     rasterDfList = list()
-    for(i in seq_len(dim(rasterStackNumFields)[[3]])){
-
-        thisRaster = rasterStackNumFields[[i]]
-        rasterName = names(thisRaster)
-        print(i)
-        print(rasterName)
-
+    for(rasterPath in rasterPaths){
+    
+        thisRasterRaw = raster::raster(rasterPath)
+        thisRaster = thisRasterRaw * hostRaster * 1000
+    
         # Sum within each polygon
         tic(msg="sum_loop")
         rasterPolySumDf = exactextractr::exact_extract(
@@ -71,9 +58,9 @@ x = peakRAM::peakRAM({
         )
         
         rasterPolySumDf = dplyr::rename(rasterPolySumDf, "raster_num_fields" = 2)
-
+    
         toc()
-
+    
         # Calc num cells with any infection
         tic(msg="pop_loop")
         rasterPolyNumPopulatedDf = exactextractr::exact_extract(
@@ -83,25 +70,25 @@ x = peakRAM::peakRAM({
             stack_apply=TRUE, 
             append_cols=c("POLY_ID")
         )
-
+    
         rasterPolyNumPopulatedDf = dplyr::rename(rasterPolyNumPopulatedDf, "raster_num_cells_populated" = 2)
-
+    
         toc()
         
         mergedDf = dplyr::full_join(rasterPolySumDf, rasterPolyNumPopulatedDf, by="POLY_ID")
         
-        rasterDfList[[rasterName]] = mergedDf
-
+        rasterDfList[[rasterPath]] = mergedDf
+    
     }
     toc()
-
+    
     # Drop poly geom to speed up
     sf::st_geometry(polyDf) = NULL
-
+    
     # For each raster = columns of extracted dfs
     dfList = list()
     for(rasterPath in rasterPaths){
-
+    
         rasterPathNorm = normalizePath(rasterPath)
         print(rasterPathNorm)
         
@@ -118,11 +105,11 @@ x = peakRAM::peakRAM({
         raster_type = splitFilename[[5]]
             
         # Pull out corresponding raster stats df
-        rasterStatsDf = rasterDfList[[rasterFilenameNoExt]]
-
+        rasterStatsDf = rasterDfList[[rasterPath]]
+    
         # Join by matching column
         rasterStatsPolyDfPartial = dplyr::full_join(polyDf, rasterStatsDf, by="POLY_ID")
-
+    
         # Calc prop fields
         raster_prop_fields = rasterStatsPolyDfPartial$raster_num_fields / rasterStatsPolyDfPartial$cassava_host_num_fields
         raster_prop_fields[is.na(raster_prop_fields)] = 0
@@ -143,14 +130,14 @@ x = peakRAM::peakRAM({
         dfList[[rasterPathNorm]] = thisRasterDf
         
     }
-
+    
     outDf = dplyr::bind_rows(dfList)
-
+    
     outPath = here::here(jobDir, "raster_poly_stats.rds")
     print(outPath)
-
+    
     saveRDS(outDf, outPath)
-
+    
     toc()
 })
 
