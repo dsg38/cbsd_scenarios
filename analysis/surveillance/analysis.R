f87bd802ecd9c77d4b72a070762da38366bfad57
David Godding <dgodding7@gmail.com>
2021-08-18 11:01:53 +0100

    Refactoring so job/batch parsing happens earlier

--- analysis/surveillance/analysis.R
+++ analysis/surveillance/analysis.R
@@ -1,27 +1,4 @@
-survey_df_raw = readRDS("./temp/big.rds")
-
-# inf_raster_name = survey_df$inf_raster_name[[1]]
-
-getJobBatch = function(inf_raster_name){
-    splitList = strsplit(inf_raster_name, "-")[[1]]
-    batch = splitList[[1]]
-    job = splitList[[2]]
-    year = as.numeric(tools::file_path_sans_ext(splitList[[4]]))
-    
-    outRow = data.frame(
-        batch=batch,
-        job=job,
-        year
-    )
-    
-    return(outRow)
-}
-
-job_batch_list = pbapply::pblapply(survey_df_raw$inf_raster_name, getJobBatch)
-
-job_batch_cols = dplyr::bind_rows(job_batch_list)
-
-survey_df = dplyr::bind_cols(job_batch_cols, survey_df_raw)
+survey_df = readRDS("./temp/big_OLD.rds")
 
 survey_df_split = split(survey_df, list(survey_df$batch, survey_df$job), drop=TRUE)
 
@@ -32,8 +9,12 @@ getDetectionYear = function(thisSurveyDf){
     arrival_year = min(thisSurveyDf$year)
     thisSurveyDfPos = thisSurveyDf[thisSurveyDf$num_positive_surveys > 0,]
     
-    # TODO: Add in contingency for if no detection
-    detect_year = min(thisSurveyDfPos$year)
+    # Contingency for if no detection within num years being analysed
+    if(nrow(thisSurveyDfPos)>0){
+        detect_year = min(thisSurveyDfPos$year)
+    }else{
+        detect_year = NA
+    }
     
     outRow = data.frame(
         batch=thisSurveyDf$batch[[1]],
--- analysis/surveillance/utils_survey.R
+++ analysis/surveillance/utils_survey.R
@@ -8,6 +8,76 @@ gen_ceil_raster = function(raster_path){
     return(raster_layer)
 }
 
+# Parse the raster file name
+getJobBatch = function(inf_raster_name){
+    splitList = strsplit(inf_raster_name, "-")[[1]]
+    batch = splitList[[1]]
+    job = splitList[[2]]
+    year = as.numeric(tools::file_path_sans_ext(splitList[[4]]))
+    
+    description_cols = data.frame(
+        inf_raster_name=inf_raster_name,
+        batch=batch,
+        job=job,
+        year
+    )
+    
+    return(description_cols)
+}
+
+# Carry out survey(s) in a single raster cell
+do_raster_cell_survey = function(
+    inf_raster_name,
+    num_surveys,
+    raster_index,
+    inf_raster,
+    host_raster
+){
+    
+    inf_raster_val = inf_raster[raster_index]
+    if(!is.numeric(inf_raster_val)){
+        inf_raster_val = 0
+    }
+    
+    host_raster_val = host_raster[raster_index]
+    
+    num_neg = host_raster_val - inf_raster_val
+
+    # Random sample from hypergeometric distribution
+    # nn = num replicates = 1
+    # m = POS
+    # n = NEG
+    # k = num surveys in cell
+    # Returns num positive samples
+    num_positive_surveys = stats::rhyper(
+        nn=1,
+        m=inf_raster_val,
+        n=num_neg,
+        k=num_surveys
+    )
+
+    # Parse raster name
+    description_cols = getJobBatch(inf_raster_name)
+    
+    # Build data cols
+    survey_result_row = data.frame(
+        raster_index=raster_index,
+        num_fields_ceil=host_raster_val,
+        num_infected_fields_ceil=inf_raster_val,
+        num_surveys=num_surveys,
+        num_positive_surveys=num_positive_surveys
+    )
+
+    out_row = dplyr::bind_cols(
+        description_cols,
+        survey_result_row
+    )
+
+    return(out_row)
+    
+}
+
+
 #' @export
 do_full_survey = function(
     inf_raster_path,
@@ -16,47 +86,6 @@ do_full_survey = function(
 ){
 
     inf_raster = gen_ceil_raster(inf_raster_path)
-
-    do_raster_cell_survey = function(
-        inf_raster_name,
-        num_surveys,
-        raster_index,
-        inf_raster,
-        host_raster
-    ){
-        
-        inf_raster_val = inf_raster[raster_index]
-        if(!is.numeric(inf_raster_val)){
-            inf_raster_val = 0
-        }
-        
-        host_raster_val = host_raster[raster_index]
-        
-        num_neg = host_raster_val - inf_raster_val
-
-        # Random sample from hypergeometric distribution
-        # nn = num replicates = 1
-        # m = POS
-        # n = NEG
-        # k = num surveys in cell
-        # Returns num positive samples
-        num_positive_surveys = stats::rhyper(
-            nn=1,
-            m=inf_raster_val,
-            n=num_neg,
-            k=num_surveys
-        )
-        
-        survey_result_row = data.frame(
-            inf_raster_name=inf_raster_name,
-            raster_index=raster_index,
-            num_fields_ceil=host_raster_val,
-            num_infected_fields_ceil=inf_raster_val,
-            num_surveys=num_surveys,
-            num_positive_surveys=num_positive_surveys
-        )
-        
-    }
 
     inf_raster_name = basename(inf_raster_path)
 
