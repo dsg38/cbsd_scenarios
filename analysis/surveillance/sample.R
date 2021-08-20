ddfca72abcae824c3c54d242fdbcff6b100b120f
David Godding <dgodding7@gmail.com>
2021-08-20 15:27:57 +0100

    Adding in 0.15 error

--- analysis/surveillance/sample.R
+++ analysis/surveillance/sample.R
@@ -15,9 +15,30 @@ host_raster = utils_survey$gen_ceil_raster(host_raster_path)
 
 inf_raster_paths = list.files(inf_raster_dir, recursive = TRUE, full.names = TRUE)
 
-survey_df_list = pbapply::pblapply(inf_raster_paths, FUN=utils_survey$do_full_survey, host_raster=host_raster, raster_survey_df=raster_survey_df)
+survey_df_list = pbapply::pblapply(
+    inf_raster_paths, 
+    FUN=utils_survey$do_full_survey, 
+    host_raster=host_raster, 
+    raster_survey_df=raster_survey_df
+)
 
 survey_df = dplyr::bind_rows(survey_df_list) %>%
     dplyr::left_join(year_mapping_df, by=c("batch", "job", "raster_year"))
 
-saveRDS(survey_df, "./outputs/2021_03_26_cross_continental/results/big.rds")
+# Add in false negative rate
+false_neg_prob = 0.15
+
+calcSurveyFalseNeg = function(
+    survey_num_pos,
+    false_neg_prob
+){
+    survey_num_pos_drop = sum(runif(n=survey_num_pos) > false_neg_prob)
+    return(survey_num_pos_drop)
+}
+
+num_positive_surveys_0.15 = sapply(survey_df$num_positive_surveys_0.00, FUN=calcSurveyFalseNeg, false_neg_prob=false_neg_prob)
+
+# Append
+survey_df_out = dplyr::bind_cols(survey_df, num_positive_surveys_0.15=num_positive_surveys_0.15)
+
+saveRDS(survey_df_out, "./outputs/2021_03_26_cross_continental/results/big.rds")
--- analysis/surveillance/temp.R
+++ analysis/surveillance/temp.R
@@ -2,12 +2,27 @@ box::use(dplyr[`%>%`])
 
 # detectionDf = read.csv("./outputs//2021_03_26_cross_continental/results/detection_year.csv")
 
-oldDf = readRDS("./outputs/2021_03_26_cross_continental/results/big_OLW.rds")
+oldDf = readRDS("./outputs/2021_03_26_cross_continental/results/big_nearest.rds")
 
-year_mapping_df = read.csv("./outputs/2021_03_26_cross_continental/sim_subset/year_mapping.csv")
+
+
+# year_mapping_df = read.csv("./outputs/2021_03_26_cross_continental/sim_subset/year_mapping.csv")
 
 survey_df = oldDf %>%
-    dplyr::rename(raster_year=year) %>%
-    dplyr::left_join(year_mapping_df, by=c("batch", "job", "raster_year"))
+    dplyr::rename(num_positive_surveys_0.00=num_positive_surveys)
 
-saveRDS(survey_df, "./outputs/2021_03_26_cross_continental/results/big.rds")
+false_neg_prob = 0.15
+
+calcSurveyFalseNeg = function(
+    survey_num_pos,
+    false_neg_prob
+){
+    survey_num_pos_drop = sum(runif(n=survey_num_pos) > false_neg_prob)
+    return(survey_num_pos_drop)
+}
+
+num_positive_surveys_0.15 = sapply(survey_df$num_positive_surveys_0.00, FUN=calcSurveyFalseNeg, false_neg_prob=false_neg_prob)
+
+outDf = dplyr::bind_cols(survey_df, num_positive_surveys_0.15=num_positive_surveys_0.15)
+
+saveRDS(outDf, "./outputs/2021_03_26_cross_continental/results/big.rds")
