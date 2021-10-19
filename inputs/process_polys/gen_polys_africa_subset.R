db9478e354db85e554336ea2cea6f92d7c6aff55
David Godding <dgodding7@gmail.com>
2021-10-19 12:22:16 +0100

    New poly of just relevent cassava countries

---
+++ inputs/process_polys/gen_polys_africa_subset.R
@@ -0,0 +1,7 @@
+box::use(utils_epidem/utils_epidem)
+
+polys_1_df = sf::read_sf("../inputs_raw/polygons/polys_1_host_default.gpkg")
+
+africaCassavaPolysDf = dplyr::filter(polys_1_df, GID_0 %in% utils_epidem$africaCassavaCountryCodes)
+
+sf::write_sf(africaCassavaPolysDf, "../inputs_raw/polygons/polys_cassava_host_default.gpkg")
--- interactive/survey_data/shiny_fast_single_slider.R
+++ interactive/survey_data/shiny_fast_single_slider.R
@@ -6,7 +6,9 @@ library(leaflet)
 surveyDf = sf::read_sf("../../../cassava_data/data_merged/data/2021_10_01/cassava_data_minimal.gpkg")
 
 # Drop all NAs
-surveyDf = surveyDf[!is.na(surveyDf$cbsd_any_bool),]
+# surveyDf = surveyDf[!is.na(surveyDf$cbsd_any_bool),]
+
+# oldDf = sf::read
 
 bbox = sf::st_bbox(surveyDf)
 
@@ -17,15 +19,9 @@ pal = colorFactor(
 
 
 ui <- bootstrapPage(
-    tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-    leafletOutput("map", width = "100%", height = "100%"),
-    absolutePanel(top = 10, right = 10,
-        # sliderInput("range", "Magnitudes", min(surveyDf$year), max(surveyDf$year), sep="",
-        #     value = range(surveyDf$year), 
-        #    step = 1,
-        #     dragRange=TRUE,
-        #     animate=list(interval=2000)
-        # )
+        tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+        leafletOutput("map", width = "100%", height = "100%"),
+        absolutePanel(top = 10, right = 10,
 
         sliderInput("integer", "Integer:",
             min(surveyDf$year), max(surveyDf$year),
@@ -33,14 +29,21 @@ ui <- bootstrapPage(
             sep="",
             value = min(surveyDf$year),
             animate=list(interval=2000)
-        )
+        ),
+        checkboxInput("somevalue", "New data", TRUE),
     )
 )
 
 server <- function(input, output, session) {
 
     filteredData <- reactive({
+        req(input$somevalue)
+        if(input$somevalue==TRUE){
             surveyDf[surveyDf$year==input$integer,]
+        }else{
+            surveyDf[1,]
+        }
+        
     })
 
     output$map <- renderLeaflet({
@@ -54,7 +57,12 @@ server <- function(input, output, session) {
             leafgl::clearGlLayers() %>%
             leafgl::addGlPoints(filteredData(), fillColor = ~pal(cbsd_any_bool))
     })
+    
 
+    x <- reactive({
+        print(input$somevalue)
+    })
+    
 }
 
 shinyApp(ui, server)
