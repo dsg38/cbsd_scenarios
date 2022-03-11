213c23fc13156232e14668d892b104c60706348f
David Godding <dgodding7@gmail.com>
2022-03-11 17:36:46 +0000

    Setting up endemic processing

--- analysis/package_inf_rasters/aggregate.R
+++ analysis/package_inf_rasters/aggregate.R
@@ -1,6 +1,8 @@
 args = commandArgs(trailingOnly=TRUE)
 
 configPath = args[[1]]
+aggFinishedBool = as.logical(as.numeric(args[[2]]))
+
 # configPath = "./2021_03_26_cross_continental/config_poly.json"
 
 config = rjson::fromJSON(file=configPath)
@@ -9,29 +11,38 @@ batchPath = here::here(config[["batchPath"]])
 outPath = here::here(batchPath, "raster_poly_stats_agg.rds")
 outPathMinimal = here::here(batchPath, "raster_poly_stats_agg_minimal.rds")
 
-progDfPath = here::here(batchPath, "progress.csv")
 
 # --------------------------------------------------------------------------
 
 # Only agg finished
-progDf = read.csv(progDfPath)
-
-doneDf = progDf[progDf$dpcLastSimTime==progDf$simEndTime,]
-
 statsDfPathsAll = list.files(batchPath, pattern="raster_poly_stats.rds", full.names = T, recursive = T)
 
-statsDfPaths = c()
-for(thisPath in statsDfPathsAll){
-    splitPath = strsplit(thisPath, "*/")[[1]]
-    
-    job = dplyr::nth(splitPath, -4)
-    
-    if(job%in%doneDf$jobName){
-        statsDfPaths = c(statsDfPaths, thisPath)
-    }
-    
-}
+if(aggFinishedBool){
 
+    progDfPath = here::here(batchPath, "progress.csv")
+
+    progDf = read.csv(progDfPath)
+
+    doneDf = progDf[progDf$dpcLastSimTime==progDf$simEndTime,]
+
+    statsDfPaths = c()
+    for(thisPath in statsDfPathsAll){
+        splitPath = strsplit(thisPath, "*/")[[1]]
+        
+        job = dplyr::nth(splitPath, -4)
+        
+        if(job%in%doneDf$jobName){
+            statsDfPaths = c(statsDfPaths, thisPath)
+        }
+        
+    }
+
+}else{
+
+
+    statsDfPaths = statsDfPathsAll
+
+}
 # --------------------------------
 
 
