bf7decd1ef2d1658561c8905c01d6cf7d7fe7c35
David Godding <dgodding7@gmail.com>
2022-05-17 11:57:20 +0100

    Adding in details of first observations beyond uganda

---
+++ figures/paper_cross_continental/cbsd_reports/plot.R
@@ -0,0 +1,4 @@
+polyDf = sf::read_sf("../../../inputs/inputs_raw/polygons/polys_cross_continental_constraints_host_CassavaMap.gpkg")
+
+targetYearDf = read.csv("../../../analysis/results/2022_03_15_cross_continental_endemic/2022_03_20_batch_0/config/inf_target_years.csv")
+
