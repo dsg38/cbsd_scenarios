e5687c44ce25e6997eed70ae614d67d218b4bbeb
David Godding <dgodding7@gmail.com>
2022-05-23 17:17:30 +0100

    Utils: Deleting checkpoint files script prog

---
+++ utils/delete_checkpoint.R
@@ -0,0 +1,6 @@
+topDir = "./"
+
+endStateFileName = "O_0_L_0_INFECTIOUS_2054.000000.tif"
+
+txtRasterPaths = normalizePath(list.files(topDir, pattern=endStateFileName, full.names = T, recursive = T))
+
