b5f84cd0917f8bce7a41ceec83d2afd39377d17e
David Godding <dgodding7@gmail.com>
2022-06-14 11:58:53 +0100

    Prog on cross continental analysis

--- sim_time.R
+++
@@ -1,6 +0,0 @@
-progressDf = read.csv("./simulations/sim_output/2022_03_15_cross_continental_endemic/2022_03_20_batch_0/progress.csv")
-
-timeHours = progressDf$simWallClockDurationSeconds / 60 / 60
-
-hist(timeHours)
-max(timeHours)
