884f09c17020ce78efa015ab71fc81d54e36f88c
David Godding <dgodding7@gmail.com>
2022-06-23 15:10:42 +0100

    Refactor surveillance stats agg due to memory issues

--- analysis/results/2022_05_16_cross_continental_endemic/2022_05_16_batch_0/3_surveillance_agg.sh
+++ analysis/results/2022_05_16_cross_continental_endemic/2022_05_16_batch_0/3_surveillance_agg.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-Rscript "./UTIL_surveillance_agg.R" 1 1000 0
+# Rscript "./UTIL_surveillance_agg.R" 1 1000 0
+Rscript "./UTIL_surveillance_agg.R" 1001 3000 1
+# Rscript "./UTIL_surveillance_agg.R" 3001 5000 2
+# Rscript "./UTIL_surveillance_agg.R" 5001 7000 3
+# Rscript "./UTIL_surveillance_agg.R" 7001 9000 4
+# Rscript "./UTIL_surveillance_agg.R" 9001 10000 5
