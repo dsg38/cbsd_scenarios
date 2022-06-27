c48d591f868f985d16248977457569021bb34e9f
David Godding <dgodding7@gmail.com>
2022-06-27 14:36:24 +0100

    Risk raster pipeline prog

--- analysis/results/2022_05_16_cross_continental_endemic/2022_05_16_batch_0/risk_rasters/0_copy_rasters_raw.R
+++ analysis/results/2022_05_16_cross_continental_endemic/2022_05_16_batch_0/risk_rasters/0_copy_rasters_raw.R
@@ -1,69 +1,30 @@
 args = commandArgs(trailingOnly=TRUE)
 
 simDir = "../../../../../simulations/sim_output/2022_05_16_cross_continental_endemic/2022_05_16_batch_0/"
+
 rasterYear = args[[1]]
+numJobs = as.numeric(args[[2]])
 
 # rasterYear = 2023
+# numJobs = 10000
 
 # Def out dir
 outDir = file.path("./raw/", paste0("inf_rasters_", rasterYear))
 dir.create(outDir, recursive = TRUE, showWarnings = FALSE)
 
-# List target rasters
-rasterPaths = list.files(simDir, paste0("O_0_L_0_INFECTIOUS_", rasterYear, ".000000.tif"), recursive = TRUE, full.names = TRUE)
-
-if(any(stringr::str_detect(rasterPaths, ".xml"))){
-    stop("Stopped as read in xml metadata files")
-}
-
-print("About to copy:")
-print(length(rasterPaths))
-
 print("Copying")
-i = 0
-for(rasterPath in rasterPaths){
-    print(i)
+for(i in seq(0, numJobs-1)){
+
+    job = paste0("job", i)
     
-    outPath = file.path(outDir, basename(rasterPath))
+    rasterPath = file.path(simDir, job, "output/runfolder0/", paste0("O_0_L_0_INFECTIOUS_", rasterYear, ".000000.tif"))
+    
+    if(file.exists(rasterPath)){
+        print(job)
+    }
+    
+    outPath = file.path(outDir, paste0(job, "-", basename(rasterPath)))
     
     file.copy(from=rasterPath, to=outPath, overwrite = TRUE)
-    
-    i = i + 1
-    
+
 }
-
-
-
-
-
-
-
-
-
-#: Pull out rasters that pass target criteria
-
-
-
-
-# rasterStack = raster::stack(rasterPaths)
-
-# # Convert each raster to bool presence/absence
-
-
-
-# # Calc mean = prob that a given raster cell is infected at a given time
-
-
-# tic()
-# sumRaster = raster::calc(rasterStack, mean, na.rm=TRUE)
-# toc()
-
-# tic()
-# x = mean(rasterStack, na.rm=TRUE)
-# toc()
-# # "../../../../simulations/sim_output/2022_05_16_cross_continental_endemic/2022_05_16_batch_0/job0/output/runfolder0/O_0_L_0_INFECTIOUS_2023.000000.tif"
-
-# : Is sum loop faster?
-# # x = rasterStack[[1]]
-# # tic()
-# # for()
