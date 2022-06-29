82b4218b38240a4ca3d75e461f121fb3229d5d24
David Godding <dgodding7@gmail.com>
2022-06-29 17:23:24 +0100

    Ranked movie pipeline v0

--- analysis/results/2022_05_16_cross_continental_endemic/2022_05_16_batch_0/rank_plotting/1_plot_hpc.sh
+++ analysis/results/2022_05_16_cross_continental_endemic/2022_05_16_batch_0/rank_plotting/1_plot_hpc.sh
@@ -17,7 +17,7 @@
 #! Estimated runtime (job is force-stopped after if exceeded):
 #SBATCH --time=00:30:00
 #! Estimated memory needed (job is force-stopped if exceeded):
-#SBATCH --mem=3420mb
+#SBATCH --mem=6840mb
 #! Submit a job array with index values between 0 and n e.g. 0-100
 #SBATCH --array=1-414
 
