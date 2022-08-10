2dea5dfe99b1ce65e38af9ea5a25eb52951d4633
David Godding <dgodding7@gmail.com>
2022-08-10 15:20:27 +0100

    Direct intro: Setting up other 2 analysis

---
+++ analysis/results/2022_08_03_direct_intro_CIV_abidjan/2022_08_03_batch_0/2_plot.R
@@ -0,0 +1,41 @@
+library(ggplot2)
+library(ggfan)
+
+polyStatsDf = readRDS("./output/raster_poly_stats_agg_minimal.rds") 
+
+outDir = file.path("./plots/dpc_fan_intervals")
+
+dir.create(outDir, showWarnings = FALSE, recursive = TRUE)
+
+# p = ggplot(polyStatsDf, aes(x=raster_year, y=raster_prop_fields, col=job)) + 
+#     geom_line() +
+#     theme(legend.position="none")
+# p
+
+for(thisPolyId in sort(unique(polyStatsDf$POLY_ID))){
+    
+    outPath = file.path(outDir, paste0(thisPolyId, ".png"))
+    print(outPath)
+    
+    polyStatsDfSubset = polyStatsDf |>
+        dplyr::filter(POLY_ID==thisPolyId)
+    
+    p = ggplot(data=polyStatsDfSubset, aes(x=raster_year, y=raster_prop_fields)) +
+        geom_fan(intervals = seq(0,1,0.2)) +
+        # geom_fan() +
+        scale_fill_gradient(low="#5D5DF8", high="#C1C1EF") +
+        ylim(0,1)+
+        xlab("Year") + 
+        ylab("Proportion of fields with CBSD infection") +
+        theme(
+            legend.position = "none",
+            axis.text=element_text(size=12),
+            axis.title=element_text(size=12)
+        )
+
+    # p
+    
+    cowplot::save_plot(outPath, plot=p, base_asp = 1)
+    
+}
+
---
+++ analysis/results/2022_08_03_direct_intro_CMR_douala/2022_08_03_batch_0/1_inf_raster_agg_stats.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+Rscript "../../../package_inf_rasters/aggregate.R" "./config/config_paths.json" 0
