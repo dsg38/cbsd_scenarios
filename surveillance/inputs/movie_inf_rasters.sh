33ed9b7a792dcd957811a339ffedb482929a7359
David Godding <dgodding7@gmail.com>
2022-10-12 13:13:05 +0100

    Plotting inf rasters

---
+++ surveillance/inputs/movie_inf_rasters.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Example command:
+# ./movie_inf_rasters.sh ./inf_rasters/cc_NGA_year_0/
+
+topDir=$1
+
+outDir=$topDir"/movies/"
+
+mkdir $outDir
+
+outPath=$outDir'inf_rasters.mp4'
+
+inPath=$topDir'/plots/*.png'
+
+echo $inPath
+echo $outPath
+
+ffmpeg -framerate 3 -pattern_type glob -i "$inPath" "$outPath"
