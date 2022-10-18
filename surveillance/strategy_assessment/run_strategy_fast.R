dcd4f34dac580429df99fb0202c55ea65db6f8db
David Godding <dgodding7@gmail.com>
2022-10-18 13:56:48 +0100

    Refactor to sweep full combo of optimal strategies v0

--- surveillance/strategy_assessment/run_strategy_fast.R
+++ surveillance/strategy_assessment/run_strategy_fast.R
@@ -1,54 +1,16 @@
 box::use(../utils/sa)
 box::use(./utils_assessment)
-box::use(foreach[...])
-box::use(tictoc[...])
+box::use(ggplot2[...])
 
-# Read in optimal df
-scenarioName = "2022_10_07_cc_NGA_year_0"
-sweepIndexStr = "sweep_307"
-niter = 10
-
-# ----------------------------------------
-rewardRatio = 1
-# ----------------------------------------
-
-scenarioDir = here::here("surveillance/results", scenarioName)
-
-optimalDfPath = file.path(scenarioDir, "data/optimalDf.csv")
-
-optimalDf = read.csv(optimalDfPath)
-
-# Pick which sweep is the scenario to be tested
-sweepIndexInt = as.integer(stringr::str_split(sweepIndexStr, "_")[[1]][[2]])
-optimalDfTargetRow = optimalDf[optimalDf$sweep_i == sweepIndexInt,]
-
-# Pull out other rows with matching numSurveys and sweep across the detection params
-numSurveys = optimalDfTargetRow$numSurveys
-optimalDfSubset = optimalDf[optimalDf$numSurveys == numSurveys,]
-
-# Define paths
-simpleGridDfPath = file.path(scenarioDir, "data/simple_grid", paste0("simple_grid_", sweepIndexStr, ".gpkg"))
-
-inputsDir = here::here("surveillance/inputs/inf_rasters_processed", optimalDfTargetRow$inputsKey, "outputs")
-infBrickPath = file.path(inputsDir, "brick.tif")
-sumRasterPointsDfPath = file.path(inputsDir, "sumRasterMaskPointsDf.csv")
-
-# Read in simple grid strategy
-simpleGridDf = sf::read_sf(simpleGridDfPath)
-
-# Read in raster brick
-infBrick = raster::brick(infBrickPath)
-
-# Process sum inf raster centroid points to classify according to the POLY_ID of each simple grid cell
-sumRasterPointsDfGridNames = utils_assessment$classifyRasterPointsDf(
-    simpleGridDf = simpleGridDf,
-    sumRasterPointsDfPath = sumRasterPointsDfPath
-)
-
-# ----------------------------------
-# NB: Loop starts here
-# ----------------------------------
-doTrial = function(i, detectionProb){
+doTrial = function(
+    i, 
+    simpleGridDf,
+    sumRasterPointsDfGridNames,
+    numSurveys,
+    infBrick,
+    rewardRatio,
+    detectionProb
+){
 
     coordsDf = utils_assessment$genWeightedRandomCoordsDf(
         simpleGridDf=simpleGridDf,
@@ -71,28 +33,121 @@ doTrial = function(i, detectionProb){
 
 }
 
-resultsDfList = list()
-for(iRow in seq_len(nrow(optimalDfSubset))){
+# Read in optimal df
+scenarioName = "2022_10_07_cc_NGA_year_0"
+niter = 10
 
-    thisOptimalDfRow = optimalDfSubset[iRow,]
+# ----------------------------------------
+rewardRatio = 1
+# ----------------------------------------
 
-    x = unlist(pbmcapply::pbmclapply(seq(1, niter), doTrial, detectionProb=thisOptimalDfRow$detectionProb))
+scenarioDir = here::here("surveillance/results", scenarioName)
 
-    resultsDfSubset = data.frame(
-        sweep_i = thisOptimalDfRow$sweep_i,
-        vals = x
-    )
+optimalDfPath = file.path(scenarioDir, "data/optimalDf.csv")
 
-    resultsDfList[[as.character(iRow)]] = resultsDfSubset
-    
-    hist(x, xlim=c(0,1), main=thisOptimalDfRow$sweep_i)
-    abline(v=thisOptimalDfRow$objective_func_val)
+optimalDf = read.csv(optimalDfPath)
+optimalDf$sweep_i = as.character(optimalDf$sweep_i)
 
+numSurveysVec = sort(unique(optimalDf$numSurveys))
+
+for(numSurveys in numSurveysVec){
+
+    # Pull out subset to be assessed
+    optimalDfSubset = optimalDf[optimalDf$numSurveys == numSurveys,]
+    sweepIndexVec = optimalDfSubset$sweep_i
+
+    for(sweepIndex in sweepIndexVec){
+
+        sweepIndexStr = paste0("sweep_", sweepIndex)
+
+        # Pick which sweep is the scenario to be tested
+        optimalDfTargetRow = optimalDf[optimalDf$sweep_i == sweepIndex,]
+
+        # Define paths
+        simpleGridDfPath = file.path(scenarioDir, "data/simple_grid", paste0("simple_grid_", sweepIndexStr, ".gpkg"))
+
+        inputsDir = here::here("surveillance/inputs/inf_rasters_processed", optimalDfTargetRow$inputsKey, "outputs")
+        infBrickPath = file.path(inputsDir, "brick.tif")
+        sumRasterPointsDfPath = file.path(inputsDir, "sumRasterMaskPointsDf.csv")
+
+        # Read in simple grid strategy
+        simpleGridDf = sf::read_sf(simpleGridDfPath)
+
+        # Read in raster brick
+        infBrick = raster::brick(infBrickPath)
+
+        # Process sum inf raster centroid points to classify according to the POLY_ID of each simple grid cell
+        sumRasterPointsDfGridNames = utils_assessment$classifyRasterPointsDf(
+            simpleGridDf = simpleGridDf,
+            sumRasterPointsDfPath = sumRasterPointsDfPath
+        )
+
+        # ----------------------------------
+        # NB: Loop starts here
+        # ----------------------------------
+
+        resultsDfList = list()
+        diffDfList = list()
+        for(iRow in seq_len(nrow(optimalDfSubset))){
+
+            thisOptimalDfRow = optimalDfSubset[iRow,]
+
+            x = unlist(pbmcapply::pbmclapply(
+                seq(1, niter), 
+                doTrial,
+                simpleGridDf=simpleGridDf,
+                sumRasterPointsDfGridNames=sumRasterPointsDfGridNames,
+                numSurveys=numSurveys,
+                infBrick=infBrick,
+                rewardRatio=rewardRatio,
+                detectionProb=thisOptimalDfRow$detectionProb
+            ))
+
+            resultsDfSubset = data.frame(
+                sweep_i = thisOptimalDfRow$sweep_i,
+                vals = x
+            )
+
+            resultsDfList[[as.character(iRow)]] = resultsDfSubset
+            
+            # Gen diff df
+            diffDfRow = data.frame(
+                sweep_i = thisOptimalDfRow$sweep_i,
+                optimalObjVal = thisOptimalDfRow$objective_func_val,
+                sampleMean = median(resultsDfSubset$vals)
+            ) |>
+                dplyr::mutate(diff = optimalObjVal - sampleMean)
+            
+            diffDfList[[as.character(iRow)]] = diffDfRow
+
+        }
+
+        resultsDf = dplyr::bind_rows(resultsDfList)
+        diffDf = dplyr::bind_rows(diffDfList)
+
+        optimalDfNonSelf = optimalDfSubset[optimalDfSubset$sweep_i != sweepIndex,]
+
+        plottingPriority = reorder(resultsDf[,"sweep_i"], resultsDf[,"vals"], FUN=quantile, probs=0.5)
+
+        # Plot as boxplots
+        p = ggplot(resultsDf, mapping = aes(x=plottingPriority, y=vals)) +
+            geom_boxplot() +
+            geom_point(data=optimalDfNonSelf, aes(x=sweep_i, y=objective_func_val), size=5, pch=4, stroke=2, col="green") +
+            geom_point(data=optimalDfTargetRow, aes(x=sweep_i, y=objective_func_val), size=5, pch=4, stroke=2, col="red") +
+            xlab("sweep_i")
+
+        # p
+
+        # Save 
+        plotPath = here::here("surveillance/strategy_assessment/results", scenarioName, paste0("numSurveys_", numSurveys), "plots", paste0("plot_", sweepIndexStr, ".png"))
+        diffDfPath = here::here("surveillance/strategy_assessment/results", scenarioName, paste0("numSurveys_", numSurveys), "data", paste0("diff_", sweepIndexStr, ".csv"))
+
+        dir.create(dirname(plotPath), recursive = TRUE, showWarnings = FALSE)
+        dir.create(dirname(diffDfPath), recursive = TRUE, showWarnings = FALSE)
+
+        ggsave(filename=plotPath, plot=p)
+        write.csv(diffDf, diffDfPath, row.names=FALSE)
+
+    }
 
 }
-
-resultsDf = dplyr::bind_rows(resultsDfList)
-
-
-# TODO: Plot as boxplots??
-
