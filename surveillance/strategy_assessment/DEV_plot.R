c099f8ba5558d34fb6f292e7ba5d63852f283b7d
David Godding <dgodding7@gmail.com>
2022-11-15 17:13:55 +0000

    v0 plotting performance of each optimal point structure as sweep detectionProb

---
+++ surveillance/strategy_assessment/DEV_plot.R
@@ -0,0 +1,19 @@
+box::use(ggplot2[...])
+
+resDf = read.csv("./points/resDf.csv") |>
+    dplyr::filter(numSurveysTrained==1000)
+
+resDf$detectionProbTest = as.factor(resDf$detectionProbTest)
+resDf$detectionProbTrained = as.factor(resDf$detectionProbTrained)
+
+p = ggplot(data=resDf, aes(x=detectionProbTrained, y=objVal, colour=detectionProbTest, group=detectionProbTest)) +
+    geom_point() +
+    geom_line()
+p
+
+
+q = ggplot(data=resDf, aes(x=detectionProbTest, y=objVal, colour=detectionProbTrained, group=detectionProbTrained)) +
+    geom_point() +
+    geom_line()
+
+q
