e395af603a41384ce9e138a77b6e9c9a829bf8cb
David Godding <dgodding7@gmail.com>
2022-12-01 12:03:33 +0000

    Pulling down di rasters

--- surveillance/temp/plot_dpc.R
+++ surveillance/temp/plot_dpc.R
@@ -6,11 +6,12 @@ bigDf_1 = bigDf[bigDf$Time==1,]
 bigDf_2 = bigDf[bigDf$Time==2,]
 polyDf = read.csv("../inputs/inf_rasters/cc_NGA_year_0/polyDf.csv")
 
+xMin = -1E6/30
 
 p = ggplot(bigDf_1, aes(x=nPopulationsInfected_0)) + 
     geom_histogram(color="black", fill="white") +
     ggtitle(paste0("d1: ", round(median(bigDf_1$nPopulationsInfected_0), 1))) +
-    scale_x_continuous(limits = c(-1E6/30, max(polyDf$raster_num_fields)))
+    scale_x_continuous(limits = c(xMin, max(polyDf$raster_num_fields)))
 
 p
 
@@ -19,7 +20,7 @@ p
 p = ggplot(bigDf_2, aes(x=nPopulationsInfected_0)) + 
     geom_histogram(color="black", fill="white") +
     ggtitle(paste0("d2: ", round(median(bigDf_2$nPopulationsInfected_0), 1))) +
-    scale_x_continuous(limits = c(-1E6/30, max(polyDf$raster_num_fields)))
+    scale_x_continuous(limits = c(xMin, max(polyDf$raster_num_fields)))
 
 p
 # 
@@ -33,7 +34,7 @@ p
 p = ggplot(polyDf, aes(x=raster_num_fields)) + 
     geom_histogram(color="black", fill="white") +
     ggtitle(paste0("cc0: ", round(median(polyDf$raster_num_fields), 1))) +
-    scale_x_continuous(limits = c(-1E6/30, max(polyDf$raster_num_fields)))
+    scale_x_continuous(limits = c(xMin, max(polyDf$raster_num_fields)))
 
 p
 
