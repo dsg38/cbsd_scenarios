09fbf88db280d20e9dad9af151e181305b16ec67
David Godding <dgodding7@gmail.com>
2022-12-05 17:16:14 +0000

    DI: Generating motage plots of results

---
+++ surveillance/results/2022_12_01_di_NGA_year_1/3_gen_simple_clusters.R
@@ -0,0 +1,33 @@
+box::use(../../utils/utils_surveillance)
+box::use(tictoc[...])
+
+optimalDfPath = "./data/optimalDf.csv"
+sweepDirTop = "./sweep/"
+
+outDir = "./data/simple_clusters/"
+
+# --------------------------
+
+dir.create(outDir, showWarnings = FALSE, recursive = TRUE)
+
+optimalDf = read.csv(optimalDfPath)
+
+
+tic()
+for(iRow in seq_len(nrow(optimalDf))){
+    
+    print(iRow)
+    
+    thisRow = optimalDf[iRow,]
+    
+    sweepDir = file.path(sweepDirTop, paste0("sweep_", thisRow$sweep_i), "outputs")
+    
+    outPath = file.path(outDir, paste0("sweep_", thisRow$sweep_i, ".gpkg"))
+    
+    utils_surveillance$genSimpleClustersSf(
+        sweepDir = sweepDir,
+        outPath = outPath
+    )
+    
+}
+toc()
---
+++ surveillance/results/2022_12_01_di_NGA_year_1/5_gen_montage.R
@@ -0,0 +1,17 @@
+box::use(../../utils/utils_surveillance)
+
+configSweepPath = "./config_sweep.json"
+optimalDfPath = "./data/optimalDf.csv"
+individualPlotsDir = "./plots/simple_grid"
+# individualPlotsDir = "./plots/simple_clusters"
+
+outPlotPath = file.path("./plots/simple_montage", paste0("montage_", basename(individualPlotsDir), ".png"))
+
+# ----------------------------
+
+utils_surveillance$genMontage(
+    configSweepPath = configSweepPath,
+    optimalDfPath = optimalDfPath,
+    individualPlotsDir = individualPlotsDir,
+    outPlotPath = outPlotPath
+)
